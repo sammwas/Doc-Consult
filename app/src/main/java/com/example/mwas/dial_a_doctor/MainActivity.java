8a8ee90154addd1820841e4856846a5abe63c9a3
sammwas <samson.m.njuguna@gmail.com>
2017-10-02 19:27:36 +0300

    Add intent so that one can click the button and goes to another activity

--- app/src/main/java/com/example/mwas/dial_a_doctor/MainActivity.java
+++ app/src/main/java/com/example/mwas/dial_a_doctor/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.mwas.dial_a_doctor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.inputField) EditText mInputField;
     @Bind(R.id.searchButton) Button mSearchButton;
 
@@ -17,5 +19,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        mSearchButton.setOnClickListener(this);
+    }
+
+    //override the onlick method that is usually contained in the view.onClickListener interface
+    @Override
+    public void onClick(View v){
+        if(v == mSearchButton) {
+            Intent intent = new Intent(MainActivity.this,DoctorsActivity.class);
+            startActivity(intent);
+        }
     }
 }
