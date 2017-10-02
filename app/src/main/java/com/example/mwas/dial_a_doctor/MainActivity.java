4653eb29085b2f2133001050dd775e93b316f057
sammwas <samson.m.njuguna@gmail.com>
2017-10-02 19:15:29 +0300

    Bind views using the butterknife library

--- app/src/main/java/com/example/mwas/dial_a_doctor/MainActivity.java
+++ app/src/main/java/com/example/mwas/dial_a_doctor/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.mwas.dial_a_doctor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.inputField) EditText mInputField;
+    @Bind(R.id.searchButton) Button mSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
