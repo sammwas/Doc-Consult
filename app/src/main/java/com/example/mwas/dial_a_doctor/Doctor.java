8f4845b60d86ad9f7f8cf33eb6f520d3cd5bb36f
sammwas <samson.m.njuguna@gmail.com>
2017-10-03 12:40:58 +0300

    Create a model that will be used to create objects

--- app/src/main/java/com/example/mwas/dial_a_doctor/Doctor.java
+++ app/src/main/java/com/example/mwas/dial_a_doctor/Doctor.java
@@ -1,8 +1,49 @@
 package com.example.mwas.dial_a_doctor;
 
+import java.util.ArrayList;
+
 /**
  * Created by mwas on 10/3/17.
  */
 
 public class Doctor {
+    private String mFirstName;
+    private String mLastName;
+    private String mImageUrl;
+    private String mBio;
+    private ArrayList<String> mPhones = new ArrayList<>();
+    private String mSpeciality;
+
+    public Doctor(String firstName, String lastName, String imageUrl, String bio, ArrayList<String> phones, String speciality) {
+        this.mFirstName = firstName;
+        this.mLastName = lastName;
+        this.mImageUrl = imageUrl;
+        this.mBio = bio;
+        this.mPhones = phones;
+        this.mSpeciality = speciality;
+    }
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public String getLastName() {
+        return mLastName;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    public String getBio() {
+        return mBio;
+    }
+
+    public ArrayList<String > getPhones() {
+        return mPhones;
+    }
+
+    public String getSpeciality() {
+        return mSpeciality;
+    }
 }
