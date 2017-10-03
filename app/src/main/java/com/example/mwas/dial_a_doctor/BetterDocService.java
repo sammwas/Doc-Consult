ac35fc2947445c33e2d9baa83b0d7a7597be9ca4
sammwas <samson.m.njuguna@gmail.com>
2017-10-03 09:37:14 +0300

    Make a request to api url and calling it asynchronously

--- app/src/main/java/com/example/mwas/dial_a_doctor/BetterDocService.java
+++ app/src/main/java/com/example/mwas/dial_a_doctor/BetterDocService.java
@@ -1,7 +1,10 @@
 package com.example.mwas.dial_a_doctor;
 
+import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 /**
  * Created by mwas on 10/3/17.
@@ -15,5 +18,17 @@ public class BetterDocService {
         urlBuider.addQueryParameter("key", Constants.API_KEY)
                 .addQueryParameter(Constants.BETTERDOC_SEARCH_PARAM, searchParam);
         String url = urlBuider.build().toString();
+
+        //create a client
+        OkHttpClient client = new OkHttpClient();
+
+        //make a request
+        Request request = new Request.Builder()
+                          .url(url)
+                          .build();
+        //calling a request asynchronously
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+
     }
 }
